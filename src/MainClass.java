3bb0113b4884415baec74cc36bee24a0ab176b68
bobback <yurkabobakk@gmail.com>
2023-03-30 15:09:16 +0300

    AddingFiles

---
+++ src/MainClass.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        SecondClass SecondClass = new SecondClass();
+        System.out.println("~~~" + SecondClass.printAndReturnFieldValue() + "~~~");
+    }
+}
---
+++ src/SecondClass.java
@@ -0,0 +1,9 @@
+public class SecondClass {
+    private int myField = 23;
+    public int printAndReturnFieldValue() {
+
+        System.out.println("---" + myField + "---");
+
+        return myField;
+    }
+}
